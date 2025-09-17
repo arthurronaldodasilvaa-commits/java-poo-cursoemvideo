254375229de94809aff775bccf0392674ad0c1e5
Arthur Ronaldo da Silva <arthurronaldodasilvaa@gmail.com>
2025-09-16 23:24:48 -0300

    Update ExAula05.java

--- arquivos/exercicios/ExAula05/src/exaula05/ExAula05.java
+++ arquivos/exercicios/ExAula05/src/exaula05/ExAula05.java
@@ -15,6 +15,8 @@ public class ExAula05 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        contaBanco c1 = new contaBanco("CC", "Arthur");
     }
     
 }
